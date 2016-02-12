74df54ab34f18af097856c68545196254236d515
Shannon Weyrick <sweyrick@nsone.net>
2016-02-12 10:07:54 -0500

    more sprint capure and sanity checks

--- sprint.py
+++ sprint.py
@@ -14,8 +14,6 @@ Commands:
 import datetime
 import os
 import sqlite3
-import sys
-import pprint
 
 from bson import ObjectId
 from docopt import docopt
@@ -49,6 +47,7 @@ class Sprint(NS1Base):
 
     def create_tables(self):
         c = self._db.cursor()
+        c.execute('''create table if not exists version (version text primary key)''')
         c.execute('''create table if not exists lists (list_id text pimary key, name text)''')
         c.execute('''create table if not exists cards (card_id text primary key, create_date text, '''
                   '''sprint_add_date text, due_date text, labels text, name text)''')
@@ -63,17 +62,14 @@ class Sprint(NS1Base):
 
     def populate_tables(self):
         c = self._db.cursor()
-        c.execute('''select * from lists''')
-        lc = c.fetchall()
-        if len(lc) == 0:
-            # populate the lists
-            board = Board(self.client, board_id=self.SPRINT_BOARD_ID)
-            lists = board.open_lists()
-            for l in lists:
-                c.execute('''insert into lists values (?, ?)''', (l.id, l.name))
-                self.list_ids[l.name] = l.id
-        else:
-            self.list_ids = {l[1]: l[0] for l in lc}
+        # version the db
+        c.execute('insert or replace into version values (1)')
+        # ensure we have all the lists
+        board = Board(self.client, board_id=self.SPRINT_BOARD_ID)
+        lists = board.open_lists()
+        for l in lists:
+            c.execute('''insert or replace into lists values (?, ?)''', (l.id, l.name))
+            self.list_ids[l.name] = l.id
         # make sure this and next sprint are in sprints table
         c.execute('''insert or ignore into sprints values (?, ?, ?, 0, 0, 0)''',
                   (self.cur_sprint_id,
@@ -143,40 +139,49 @@ class Sprint(NS1Base):
                       (sprint_id.date(), card.list_id, card.id, snapshot_phase, 0))
         c.close()
 
-    def get_sprint_flag(self, sprint_id, name):
+    def get_sprint_flag(self, name, sprint_id):
         c = self._db.cursor()
         c.execute('''select %s from sprints where sprint_id=?''' % name, (sprint_id,))
-        if c.rowcount != 1:
-            raise Exception("Problem: can't pull finished flag")
         flag = c.fetchone()
+        if flag is None:
+            raise Exception('Unable to get %s from %s' % (name, sprint_id))
         c.close()
-        return flag
+        return flag[0]
 
-    def set_sprint_flag(self, sprint_id, name):
+    def set_sprint_flag(self, name, sprint_id):
         c = self._db.cursor()
         c.execute('''update sprints set %s=1 where sprint_id=?''' % name, (sprint_id,))
         c.close()
+
+    def ensure(self, name, sprint_id):
+        flag = self.get_sprint_flag(name, sprint_id)
+        if int(flag) != 1:
+            raise Exception("Sprint %s has not yet been %s, aborting" % (sprint_id, name))
+
+    def ensure_not(self, name, sprint_id):
+        flag = self.get_sprint_flag(name, sprint_id)
+        if int(flag) == 1:
+            raise Exception("Sprint %s has already been %s, aborting" % (sprint_id, name))
 
     def finish_sprint(self):
 
         print "Finishing Sprint %s" % self.last_sprint_id
 
-        finished = self.get_sprint_flag(self.last_sprint_id, 'finished')
-        if finished == 1:
-            raise Exception("Sprint has already been finished, aborting")
+        self.ensure('prepared', self.last_sprint_id)
+        self.ensure('started', self.last_sprint_id)
+        self.ensure_not('finished', self.last_sprint_id)
 
         # outgoing sprint: snapshot_phase=2 (finish)
-        ## capture sprint board state to sqlite (end)
         try:
             self.capture_sprint(self.last_sprint_start, snapshot_phase=2)
         except Exception as e:
             print "ROLLING BACK"
             self._db.rollback()
             raise e
-        self.set_sprint_flag('finished')
+        self.set_sprint_flag('finished', self.last_sprint_id)
         self._db.commit()
 
-        ## archive everything in Done column
+        # archive all cards in Done column
         board = Board(self.client, board_id=self.SPRINT_BOARD_ID)
         done_list = board.get_list(self.list_ids['Done'])
         done_cards = done_list.list_cards()
@@ -188,12 +193,28 @@ class Sprint(NS1Base):
 
     def prep_sprint(self):
         # incoming sprint: snapshot_phase=1 (start), from_roadmap=1
+        print "Preparing Sprint %s" % self.cur_sprint_id
+
+        self.ensure_not('prepared', self.cur_sprint_id)
+
         ## change title to todays date
+        board = Board(self.client, board_id=self.SPRINT_BOARD_ID)
+        # no method for setting name, use client directly
+        board.client.fetch_json(
+            '/boards/' + board.id + '/name',
+            http_method='PUT',
+            post_args={'value': 'Current Sprint %s' % self.cur_sprint_id, }, )
+
         ## right shift sprint roadmap, bring into current sprint
+        
         ## capture tickets coming in from roadmap (so we can figure out which were added ad hoc after sprint start)
-        pass
+
+        self.set_sprint_flag('prepared', self.cur_sprint_id)
+        self._db.commit()
 
     def start_sprint(self):
+        self.ensure('prepared', self.last_sprint_id)
+        self.ensure_not('started', self.last_sprint_id)
         # incoming sprint: snapshot_phase=1 (start), from_roadmap=0
         ## run after prep and after any manual tickets have been added, to capture sprint start
         ## capture sprint board state to sqlite (start)
@@ -209,6 +230,7 @@ class Sprint(NS1Base):
         ### num in each column
         ### num overdue
         ### avg age of open tickets
+        ### avg length in sprint
         ### num fires (label)
         # incoming
         ### num incoming from sprint roadmaps
@@ -238,11 +260,13 @@ if __name__ == "__main__":
         t.show()
     elif args['<command>'] == 'finish':
         t.finish_sprint()
-    elif args['<command>'] == 'prep':
+    elif args['<command>'] == 'prepare':
         t.prep_sprint()
     elif args['<command>'] == 'start':
         t.start_sprint()
     elif args['<command>'] == 'backup':
         t.backup()
+    else:
+        print "unknown command: %s" % args['<command>']
 
 
